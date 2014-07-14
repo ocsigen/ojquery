d7a721d48a9b67b40a733296e81c186652b76390
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-07-14 18:23:25 +0200

    Jenkins: build_doc.

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,4 +1,13 @@
+
 opam pin add --no-action ojquery .
 opam install --deps-only ojquery
 opam install --verbose ojquery
-opam remove --verbose ojquery
+
+do_build_doc () {
+  make wikidoc
+  cp -Rf _build/ojquery-api.wikidocdir/*.wiki $(API_DIR)
+}
+
+do_remove () {
+  opam remove --verbose ojquery
+}
